6d9c9e4d9743a1af82ae9ac1106deda0a9d0d93a
cknan <97368174@qq.com>
2019-11-28 11:51:24 +0800

    Update Constant.java

--- src/main/java/com/config/Constant.java
+++ src/main/java/com/config/Constant.java
@@ -7,9 +7,10 @@ public class Constant {
     /**
      * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppKey
      */
-    public static final String APP_KEY = "***";
+    public static final String APP_KEY = "dingokbx1xp4vl3ddg5a";
     /**
      * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppSecret
      */
-    public static final String APP_SECRET="***";
+    public static final String APP_SECRET="byLmTOT4Oh-Lkut8AClocG47ZjWHbcDQ6fV_QxkUD0yhbVUJZtB7s7Jga0d3aMEE";
+    server.port=8080;
 }
